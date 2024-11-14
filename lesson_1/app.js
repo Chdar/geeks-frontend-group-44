2ae4249315f56b4b3da7d77304104d8cf186961c
Chdar <vorondaria@gmail.com>
2024-11-14 21:18:28 +0600

    Add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,7 @@
+    console.log("Hello World!");
+    // alert("Hello JavaScript");
+    document.write("Hello Group 44");
+
+    document.body.style.color = "white";
+    document.body.style.fontSize = "6rem";
+    document.body.style.fontFamily = "Arial, Helvetica, sans-serif";
